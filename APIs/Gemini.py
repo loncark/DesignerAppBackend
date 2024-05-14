3874354033583bca7f9cb932145d6dc0e665daa1
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-05-14 16:04:27 +0200

    Trends works.

--- APIs/Gemini.py
+++ APIs/Gemini.py
@@ -1,9 +1,9 @@
-# START VPN ON US SERVER BEFORE RUNNING
+# START VPN ON US OR JAPAN SERVER BEFORE RUNNING
 
 import google.generativeai as genai
-from config import API_KEY
+from config import GEMINI_API_KEY
 
-genai.configure(api_key=API_KEY)
+genai.configure(api_key=GEMINI_API_KEY)
 
 model = genai.GenerativeModel('gemini-pro')
 response = model.generate_content('Tell me what cats are in one sentence.')
