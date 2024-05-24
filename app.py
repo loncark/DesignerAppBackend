a9d46caf1f356f48baea625ee04508afd33d57af
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-05-24 17:12:31 +0200

    Managed to get gemini working using blueprints

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask
-import controller.GeminiController as GeminiController
-import controller.TrademarkController as TrademarkController
-import controller.StableDiffusionController as SDController
-import controller.GoogleTrendsController as GTController
-import controller.FirebaseController as FirebaseController
+from controller.GeminiController import gemini_bp
 
 app = Flask(__name__)
 
-app.route('/gemini')(GeminiController.queryGemini)
-app.route('/trends')(GTController.queryGoogleTrends)
-app.route('/db')(FirebaseController.saveToRealtimeDb)
-app.route('/storage')(FirebaseController.saveToStorage)
-app.route('/tess')(TrademarkController.queryTESS)
-app.route('/sd')(SDController.SDtextToImage)
+app.register_blueprint(gemini_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- controller/GeminiController.py
+++ controller/GeminiController.py
@@ -1,4 +1,16 @@
 import service.GeminiService
+from flask import Blueprint, request
 
+gemini_bp = Blueprint('gemini_bp', __name__)
+
+@gemini_bp.route('/gemini', methods=['POST'])
 def queryGemini():
-    return service.GeminiService.fetchResponse('')
+    data = request.get_json()
+    prompt = data.get('prompt')
+    return service.GeminiService.fetchResponse(prompt)
+
+def generateTags():
+    return service.GeminiService.generateTags('t-shirt with a happy cat')
+
+def generateTitle():
+    return service.GeminiService.generateTitle('t-shirt with a happy cat')
