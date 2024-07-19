e57a51cebcfb7bd0600bc4b9cc8f15bc252e32ab
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-07-19 20:46:36 +0200

    Increased timeout to 50mins

--- service/StableDiffusionService.py
+++ service/StableDiffusionService.py
@@ -1,4 +1,4 @@
-import aiohttp
+import aiohttp, asyncio
 from config import SD_TXT2IMG_URL, SD_IMG2IMG_URL
 
 async def txt2img(payload):
@@ -9,13 +9,21 @@ async def img2img(payload):
   return await call_api(SD_IMG2IMG_URL, payload)
 
 
-async def call_api(url, payload):
-   async with aiohttp.ClientSession() as session:
-    async with session.post(url, json=payload) as response:
-      if response.status == 200:
-        return await response.json()
-      else:
-        print(f"Error: {response.status}")
+async def call_api(url, payload, timeout=3000):  # 50 minutes timeout
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, json=payload, timeout=aiohttp.ClientTimeout(total=timeout)) as response:
+                if response.status == 200:
+                    return await response.json()
+                else:
+                    print(f"Error: {response.status}")
+                    return None
+        except asyncio.TimeoutError:
+            print(f"Request timed out after {timeout} seconds")
+            return None
+        except aiohttp.ClientError as e:
+            print(f"Client error occurred: {e}")
+            return None
 
 
 
