0d311cc1421b7de728e3832f841a54ed1d8dcbb5
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-08-12 17:44:14 +0200

    test_filterJson passes

--- tests/test_TrademarkService.py
+++ tests/test_TrademarkService.py
@@ -1,5 +1,5 @@
 import json, requests
-import unittest
+import unittest, os
 from unittest.mock import patch, Mock
 from service.TrademarkService import fetchAndFilterResponse, fetchResponse, filterJson
 
@@ -14,6 +14,22 @@ class TestTrademarkService(unittest.TestCase):
         data = fetchResponse("prompt")
         mock_get.assert_called_with("https://uspto-trademark.p.rapidapi.com/v1/trademarkSearch/prompt/active", headers={'X-RapidAPI-Key': '39c42055c4msh632b3c92f3544ebp1ced69jsnb3f52628032e', 'X-RapidAPI-Host': 'uspto-trademark.p.rapidapi.com'})
         self.assertEqual(data, {"whatever":"whatever"})
+
+    def test_filterJson(self):
+        current_dir = os.path.dirname(__file__)
+        mockdata_path = os.path.join(current_dir, 'mockdata', 'TrademarkMock.json')
+        expected_path = os.path.join(current_dir, 'mockdata', 'TrademarkOut.json')
+
+        with open(mockdata_path, 'r') as f:
+            input_data = json.load(f)
+
+        with open(expected_path, 'r') as f:
+            expected_data = json.load(f)
+
+        result = filterJson(input_data)
+
+        self.assertEqual(result, expected_data)
+
 
 if __name__ == '__main__':
     unittest.main()
