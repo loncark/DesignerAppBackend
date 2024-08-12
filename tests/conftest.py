f4fdb838e2595f6d5733984dcc45ac9961ebecf4
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-08-12 13:43:23 +0200

    Comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,8 @@ import sys
 import os
 import pytest
 
-# Add the project root directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Now import create_app after modifying the Python path
 from app import create_app
 
 @pytest.fixture
@@ -17,4 +15,4 @@ def app():
 
 @pytest.fixture
 def client(app):
-    return app.test_client()
+    return app.test_client()    # simulates requests
