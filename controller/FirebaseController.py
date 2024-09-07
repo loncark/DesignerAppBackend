63019c72eeb4e9e353d0259ae05b0796f7ac0ec7
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-09-07 14:42:29 +0200

    Cleanup

--- controller/FirebaseController.py
+++ controller/FirebaseController.py
@@ -19,20 +19,14 @@ def getAllDesigns():
 
 @firebase_bp.route('/storage', methods=['POST'])
 def saveToStorage():
-    if 'image' not in request.files:
-        return jsonify({'error': 'No image file provided'}), 400
-
-    if 'design_id' not in request.form or not request.form['design_id']:
-        return jsonify({'error': 'No design_id provided'}), 400
-
     image = request.files['image']
-    design_id = request.form['design_id']
+    designId = request.form['design_id']
     
     try:
-        download_url = service.FirebaseService.storeToStorage(image, design_id)
-        return jsonify({'url': download_url}), 200
+        downloadUrl = service.FirebaseService.storeToStorage(image, designId)
+        return jsonify({'url': downloadUrl}), 200
     except Exception as e:
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': e}), 500
 
 
 @firebase_bp.route('/storageDelete', methods=['DELETE'])
--- service/FirebaseService.py
+++ service/FirebaseService.py
@@ -10,8 +10,8 @@ from io import BytesIO
 from zipfile import ZipFile
 
 # needs to be done only once
-cred = credentials.Certificate(FIREBASE_CREDS_JSON_LOCATION)
-firebase_admin.initialize_app(cred, {
+creds = credentials.Certificate(FIREBASE_CREDS_JSON_LOCATION)
+firebase_admin.initialize_app(creds, {
     'databaseURL': REALTIMEDB_URL,
     'storageBucket': STORAGE_BUCKET_LOCATION
 })
@@ -19,7 +19,7 @@ firebase_admin.initialize_app(cred, {
 # REALTIME DATABASE
 
 def storeDesignToDb(design_name, title, tags, related_links, image_links, description, design_id):
-    ref = db.reference('/Designs')
+    dbRef = db.reference('/Designs')
 
     data = {
         'design_name': design_name,
@@ -31,43 +31,44 @@ def storeDesignToDb(design_name, title, tags, related_links, image_links, descri
     }
 
     try:
-        design_ref = ref.child(design_id)
-        if design_ref.get():
-            design_ref.update(data)
+        designRef = dbRef.child(design_id)
+        if designRef.get():
+            designRef.update(data)
             return f"Design with ID {design_id} updated successfully"
         else:
-            design_ref.set(data)
+            designRef.set(data)
             return f"Design with ID {design_id} added successfully"
     except Exception as e:
         return f'Error uploading design data: {e}'
 
 def getAllDesigns():
-    ref = db.reference('/Designs')
+    dbRef = db.reference('/Designs')
+
     try:
-        designs = ref.get()
+        designs = dbRef.get()
         if designs:
-            designs_with_ids = []
-            for design_id, design_data in designs.items():
-                designs_with_ids.append({
-                    'design_name': design_data.get('design_name', ''),
-                    'design_id': design_id,
-                    'related_links': design_data.get('related_links', []),
-                    'image_links': design_data.get('image_links', []),
-                    'tags': design_data.get('tags', []),
-                    'title': design_data.get('title', ''),
-                    'description': design_data.get('description', '')
+            designsWithIds = []
+            for designId, designData in designs.items():
+                designsWithIds.append({
+                    'design_name': designData.get('design_name', ''),
+                    'design_id': designId,
+                    'related_links': designData.get('related_links', []),
+                    'image_links': designData.get('image_links', []),
+                    'tags': designData.get('tags', []),
+                    'title': designData.get('title', ''),
+                    'description': designData.get('description', '')
                 })
-            return designs_with_ids
+            return designsWithIds
         else:
             return []
     except Exception as e:
         return f'Error retrieving designs: {e}'
 
 def deleteDesign(design_id):
-    ref = db.reference(f'Designs/{design_id}')
+    dbRef = db.reference(f'Designs/{design_id}')
     
     try:
-        ref.delete()
+        dbRef.delete()
         return f"Design with ID {design_id} deleted successfully"
         
     except Exception as e:
@@ -80,7 +81,7 @@ def storeToStorage(image_file, design_id):
         bucket = storage.bucket()
         image = Image.open(image_file)
 
-        # Save image to a temporary location
+        # save image to a temporary location
         temp_path = '/tmp/image.png'
         image.save(temp_path, format='PNG')
 
@@ -95,17 +96,15 @@ def storeToStorage(image_file, design_id):
 def deleteFromStorageByUrl(download_url):
     try:
         bucket = storage.bucket()
-        file_name = download_url.split('/')[-2] + '/' + download_url.split('/')[-1]
-        print(file_name)
-        blob = bucket.blob(file_name)
+        fileName = download_url.split('/')[-2] + '/' + download_url.split('/')[-1]
+        blob = bucket.blob(fileName)
 
-        # Delete the file if it exists
         if blob.exists():
             blob.delete()
-            print("I deleted the image")
+            print("Image deleted.")
             return True
         else:
-            print("File not found.") 
+            print("File not found in Storage.") 
             return False
 
     except Exception as e:
@@ -115,12 +114,26 @@ def deleteFromStorageByUrl(download_url):
 
 def createDesignZip(design):
     try:
-        # Create a temporary directory
-        temp_dir = f"temp_{design['design_name']}_{design['design_id'] or 'noID'}"
-        os.makedirs(temp_dir, exist_ok=True)
+        tempDir = createTemporaryDirectory(design)
+        createAndWriteTextFile(tempDir, design)
+        downloadImages(tempDir, design)
+        memoryFile = createZip(tempDir)
+        removeTemporaryDirectory(tempDir)
 
-        # Create and write text file
-        txt_content = f"""Design name: {design['design_name']}
+        # prepare zip file for sending
+        memoryFile.seek(0)
+        return memoryFile
+    except Exception as e:
+        print(f"Error creating design zip: {e}")
+        raise
+
+def createTemporaryDirectory(design):
+    tempDir = f"temp_{design['design_name']}_{design['design_id'] or 'noID'}"
+    os.makedirs(tempDir, exist_ok=True)
+    return tempDir
+
+def createAndWriteTextFile(tempDir, design):
+    text = f"""Design name: {design['design_name']}
 Design id: {design['design_id'] or 'N/A'}
 
 Tags: {', '.join(design['tags'])}
@@ -130,31 +143,28 @@ Description: {design['description']}
 Related links:
 {os.linesep.join(design['related_links'])}"""
 
-        with open(f"{temp_dir}/design_info_{design['design_id']}.txt", 'w') as f:
-            f.write(txt_content)
+    with open(f"{tempDir}/design_info_{design['design_id']}.txt", 'w') as f:
+        f.write(text)
 
-        # Download images
-        for i, url in enumerate(design['image_links']):
-            response = requests.get(url)
-            if response.status_code == 200:
-                with open(f"{temp_dir}/image_{i+1}.png", 'wb') as f:
-                    f.write(response.content)
+def downloadImages(tempDir, design):
+    i = 0
+    for url in design['image_links']:
+        response = requests.get(url)
+        if response.status_code == 200:
+            with open(f"{tempDir}/image_{i+1}.png", 'wb') as f:
+                f.write(response.content)
+        i += 1
 
-        # Create a zip file
-        memory_file = BytesIO()
-        with ZipFile(memory_file, 'w') as zf:
-            for root, dirs, files in os.walk(temp_dir):
-                for file in files:
-                    zf.write(os.path.join(root, file), file)
+def createZip(tempDir):
+    memoryFile = BytesIO()
+    with ZipFile(memoryFile, 'w') as zf:
+        for root, dirs, files in os.walk(tempDir):
+            for file in files:
+                zf.write(os.path.join(root, file), file)
 
-        # Clean up the temporary directory
-        for file in os.listdir(temp_dir):
-            os.remove(os.path.join(temp_dir, file))
-        os.rmdir(temp_dir)
+    return memoryFile
 
-        # Prepare the zip file for sending
-        memory_file.seek(0)
-        return memory_file
-    except Exception as e:
-        print(f"Error creating design zip: {str(e)}")
-        raise
+def removeTemporaryDirectory(tempDir):
+    for file in os.listdir(tempDir):
+        os.remove(os.path.join(tempDir, file))
+    os.rmdir(tempDir)
