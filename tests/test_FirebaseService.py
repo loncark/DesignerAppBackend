c6306a61eeb2fcf76453edc9d0836469d9fa6ffa
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-09-07 17:32:09 +0200

    Cleanup

--- tests/test_FirebaseService.py
+++ tests/test_FirebaseService.py
@@ -1,285 +1,168 @@
-import unittest, os, stat, time
-from unittest.mock import patch, Mock, mock_open
-from service.FirebaseService import storeDesignToDb, getAllDesigns, deleteDesign, storeToStorage, deleteFromStorageByUrl, createDesignZip
-from io import BytesIO
-from zipfile import ZipFile
+import unittest
+from unittest.mock import patch, Mock
+from service.FirebaseService import storeDesignToDb, getAllDesigns, deleteDesign, storeToStorage, deleteFromStorageByUrl
 
 class TestFirebaseService(unittest.TestCase):
 
     @patch('firebase_admin.db.reference')
-    def test_storeDesignToDb(self, mock_reference):
-        mock_ref = Mock()
-        mock_design_ref = Mock()
-        mock_reference.return_value = mock_ref
-        mock_ref.child.return_value = mock_design_ref
+    def test_storeDesignToDb(self, mock_db_reference):
+        mockDbRef = Mock()
+        mock_db_reference.return_value = mockDbRef
+        mockDesignRef = Mock()
+        mockDbRef.child.return_value = mockDesignRef
 
         design_name = "Test Design"
         title = "Test Title"
         tags = ["tag1", "tag2"]
-        related_links = ["http://example.com"]
-        image_links = ["http://example.com/image.png"]
+        related_links = []
+        image_links = [],
+        description = "Some description."
         design_id = "12345"
 
-        # update case
-        mock_design_ref.get.return_value = True
-        result = storeDesignToDb(design_name, title, tags, related_links, image_links, design_id)
-        mock_design_ref.update.assert_called_once_with({
-            'design_name': design_name,
-            'title': title,
-            'tags': tags,
-            'related_links': related_links,
-            'image_links': image_links
-        })
-        self.assertEqual(result, f"Design with ID {design_id} updated successfully")
+        # case 1
+        mockDesignRef.get.return_value = True
+        result = storeDesignToDb(design_name, title, tags, related_links, image_links, description, design_id)
 
-        mock_design_ref.reset_mock()
+        mockDesignRef.update.assert_called_once()
+        self.assertEqual(result, "Design with ID 12345 updated successfully")
 
-        # create
-        mock_design_ref.get.return_value = None
-        result = storeDesignToDb(design_name, title, tags, related_links, image_links, design_id)
-        mock_design_ref.set.assert_called_once_with({
-            'design_name': design_name,
-            'title': title,
-            'tags': tags,
-            'related_links': related_links,
-            'image_links': image_links
-        })
-        self.assertEqual(result, f"Design with ID {design_id} added successfully")
+        # case 2
+        mockDesignRef.get.return_value = False
+        result = storeDesignToDb(design_name, title, tags, related_links, image_links, description, design_id)
+
+        mockDesignRef.set.assert_called_once()
+        self.assertEqual(result, "Design with ID 12345 added successfully")
+
 
     @patch('firebase_admin.db.reference')
-    def test_getAllDesigns(self, mock_reference):
-        mock_ref = Mock()
-        mock_reference.return_value = mock_ref
-
-        mock_designs = {
+    def test_getAllDesigns(self, mock_db_reference):
+        mockDbRef = Mock()
+        mock_db_reference.return_value = mockDbRef
+        mockDesigns = {
             '12345': {
-                'design_name': 'Design 1',
-                'related_links': ['http://example.com/1'],
-                'image_links': ['http://example.com/image1.png'],
-                'tags': ['tag1', 'tag2'],
-                'title': 'Title 1'
+                'design_name':'Some name'
             },
-            '67890': {
-                'design_name': 'Design 2',
-                'related_links': ['http://example.com/2'],
-                'image_links': ['http://example.com/image2.png'],
-                'tags': ['tag3', 'tag4'],
-                'title': 'Title 2'
+            '67890':{
+                'description':'Some description'
             }
         }
-
-        mock_ref.get.return_value = mock_designs
-
-        result = getAllDesigns()
-
-        expected = [
+        
+        # case 1
+        mockDbRef.get.return_value = mockDesigns
+        expectedResult = [
             {
-                'design_name': 'Design 1',
+                'description': '',
                 'design_id': '12345',
-                'related_links': ['http://example.com/1'],
-                'image_links': ['http://example.com/image1.png'],
-                'tags': ['tag1', 'tag2'],
-                'title': 'Title 1'
+                'design_name': 'Some name',
+                'image_links': [],
+                'related_links': [],
+                'tags': [],
+                'title': ''
             },
             {
-                'design_name': 'Design 2',
+                'description':'Some description',
                 'design_id': '67890',
-                'related_links': ['http://example.com/2'],
-                'image_links': ['http://example.com/image2.png'],
-                'tags': ['tag3', 'tag4'],
-                'title': 'Title 2'
+                'design_name': '',
+                'image_links': [],
+                'related_links': [],
+                'tags': [],
+                'title': ''
             }
         ]
 
-        self.assertEqual(result, expected)
-
-        # no designs case
-        mock_ref.get.return_value = None
         result = getAllDesigns()
-        self.assertEqual(result, [])
+        
+        self.assertEqual(result, expectedResult)
 
-        # exception handling
-        mock_ref.get.side_effect = Exception("Test exception")
+        # case 2
+        mockDbRef.get.return_value = None
+        expectedResult = []
+
         result = getAllDesigns()
-        self.assertEqual(result, 'Error retrieving designs: Test exception')
+
+        self.assertEqual(result, expectedResult)
+
+        # case 3
+        mockDbRef.get.side_effect = Exception("Test exception")
+        result = getAllDesigns()
+        self.assertEqual(result, "Error retrieving designs: Test exception")
+
 
     @patch('firebase_admin.db.reference')
-    def test_deleteDesign(self, mock_reference):
-        mock_ref = Mock()
-        mock_reference.return_value = mock_ref
+    def test_deleteDesign(self, mock_db_reference):
+        mockDbRef = Mock()
+        mock_db_reference.return_value = mockDbRef
 
-        # success
-        design_id = '12345'
-        result = deleteDesign(design_id)
-        mock_ref.delete.assert_called_once()
-        self.assertEqual(result, f"Design with ID {design_id} deleted successfully")
+        # case 1
+        result = deleteDesign('12345')
+        mockDbRef.delete.assert_called_once()
+        self.assertEqual(result, "Design with ID 12345 deleted successfully")
 
-        # exception
-        mock_ref.delete.side_effect = Exception("Test exception")
-        result = deleteDesign(design_id)
+        # case 2
+        mockDbRef.delete.side_effect = Exception("Test exception")
+        result = deleteDesign('12345')
         self.assertEqual(result, 'Error deleting design data: Test exception')
+
 
     @patch('firebase_admin.storage.bucket')
     @patch('PIL.Image.open')
     @patch('uuid.uuid4')
     def test_storeToStorage(self, mock_uuid, mock_image_open, mock_storage_bucket):
-            mock_image = Mock()
-            mock_image_open.return_value = mock_image
-            mock_uuid.return_value = 'test-uuid'
+            mockBucket = Mock()
+            mock_storage_bucket.return_value = mockBucket
+            mockImage = Mock()
+            mock_image_open.return_value = mockImage
+            mockBlob = Mock()
+            mockBucket.blob.return_value = mockBlob
+            mock_uuid.return_value = 'testId'
+
+            # case 1
+            mockBlob.public_url = 'test-url'
+
+            result = storeToStorage('test.png', '12345')
+
+            mockImage.save.assert_called_once_with('/tmp/image.png', format='PNG')
+            mockBucket.blob.assert_called_once_with(f'images/12345/testId.png')
+            mockBlob.upload_from_filename.assert_called_once_with('/tmp/image.png')
+            mockBlob.make_public.assert_called_once()
             
-            mock_bucket = Mock()
-            mock_blob = Mock()
-            mock_bucket.blob.return_value = mock_blob
-            mock_storage_bucket.return_value = mock_bucket
+            self.assertEqual(result, 'test-url')
             
-            mock_blob.public_url = 'https://example.com/test-image.png'
+            # case 2
+            mockImage.save.side_effect = Exception("Test exception")
             
-            image_file = 'test_image.png'
-            design_id = '12345'
-            temp_path = '/tmp/image.png'
+            result = storeToStorage('test.png', '12345')
             
-            result = storeToStorage(image_file, design_id)
-            
-            mock_image.save.assert_called_once_with(temp_path, format='PNG')
-            mock_bucket.blob.assert_called_once_with(f'images/{design_id}/test-uuid.png')
-            mock_blob.upload_from_filename.assert_called_once_with(temp_path)
-            mock_blob.make_public.assert_called_once()
-            
-            self.assertEqual(result, 'https://example.com/test-image.png')
-            
-            # exception case
-            mock_image.save.side_effect = Exception("Test exception")
-            result = storeToStorage(image_file, design_id)
             self.assertEqual(result, 'Error uploading image: Test exception')
 
     @patch('firebase_admin.storage.bucket')
     def test_deleteFromStorageByUrl(self, mock_storage_bucket):
-        download_url = "https://storage.googleapis.com/bucket_name/folder/file.txt"
+        mockBucket = Mock()
+        mock_storage_bucket.return_value = mockBucket
+        mockBlob = Mock()
+        mockBucket.blob.return_value = mockBlob
+        testUrl = "https://storage.googleapis.com/bucket_name/folder/file.txt"
 
-        mock_bucket = Mock()
-        mock_blob = Mock()
+        # case 1
+        mockBlob.exists.return_value = True
 
-        mock_storage_bucket.return_value = mock_bucket
-        mock_bucket.blob.return_value = mock_blob
-        mock_blob.exists.return_value = True  
+        result = deleteFromStorageByUrl(testUrl)
+        
+        mockBucket.blob.assert_called_once_with('folder/file.txt')
+        mockBlob.delete.assert_called_once()
+        self.assertTrue(result)
 
-        result = deleteFromStorageByUrl(download_url)
+        # case 2
+        mockBlob.exists.return_value = False
+        
+        result = deleteFromStorageByUrl(testUrl)
 
-        mock_storage_bucket.assert_called_once() 
-        mock_bucket.blob.assert_called_once_with('folder/file.txt') 
-        mock_blob.exists.assert_called_once()  
-        mock_blob.delete.assert_called_once()  
-        self.assertTrue(result) 
-
-        # file does not exist case
-        mock_storage_bucket.reset_mock()
-        mock_bucket.reset_mock()
-        mock_blob.reset_mock()
-
-        mock_storage_bucket.return_value = mock_bucket
-        mock_bucket.blob.return_value = mock_blob
-        mock_blob.exists.return_value = False
-
-        result = deleteFromStorageByUrl(download_url)
-
-        mock_storage_bucket.assert_called_once()
-        mock_bucket.blob.assert_called_once_with('folder/file.txt')
-        mock_blob.exists.assert_called_once()
-        mock_blob.delete.assert_not_called()
         self.assertFalse(result)
+        
+        # case 3
+        mockBlob.delete.side_effect = Exception("Test exception")
 
-
-    @patch('os.makedirs')
-    @patch('builtins.open', new_callable=mock_open)
-    @patch('requests.get')
-    @patch('os.walk')
-    @patch('os.listdir')
-    @patch('os.remove')
-    @patch('os.rmdir')
-    @patch('zipfile.ZipFile')
-    @patch('os.stat')
-    def test_createDesignZip(self, mock_stat, mock_zipfile, mock_rmdir, mock_remove, 
-                             mock_listdir, mock_walk, mock_requests_get, mock_file, 
-                             mock_makedirs):
-        design = {
-            'design_name': 'Test Design',
-            'design_id': '123',
-            'tags': ['tag1', 'tag2'],
-            'title': 'Test Title',
-            'related_links': ['http://link1.com', 'http://link2.com'],
-            'image_links': ['http://image1.png', 'http://image2.png']
-        }
-
-        mock_response = Mock()
-        mock_response.status_code = 200
-        mock_response.content = b'fake image content'
-        mock_requests_get.return_value = mock_response
-
-        temp_dir = os.path.join('temp_Test Design_123')
-        mock_walk.return_value = [
-            (temp_dir, [], ['design_info_123.txt', 'image_1.png', 'image_2.png'])
-        ]
-
-        mock_listdir.return_value = ['design_info_123.txt', 'image_1.png', 'image_2.png']
-
-        current_time = int(time.time())
-        mock_stat.return_value = Mock(
-            st_mode=stat.S_IFREG,  # This indicates a regular file
-            st_mtime=current_time, 
-            st_size=1000,
-            st_atime=current_time,
-            st_ctime=current_time
-        )
-
-        result = createDesignZip(design)
-
-        mock_makedirs.assert_called_once_with(temp_dir, exist_ok=True)
-
-        expected_content = f"""Design name: Test Design
-Design id: 123
-Tags: tag1, tag2
-Title: Test Title
-Related links:
-http://link1.com
-http://link2.com"""
-        mock_file.assert_any_call(f"{temp_dir}/design_info_123.txt", 'w')
-        #mock_file().write.assert_any_call(expected_content) 
-        #does not pass for some strange reason
-
-        # check images downloaded and written
-        mock_requests_get.assert_any_call('http://image1.png')
-        mock_requests_get.assert_any_call('http://image2.png')
-        mock_file.assert_any_call(f"{temp_dir}/image_1.png", 'wb')
-        mock_file.assert_any_call(f"{temp_dir}/image_2.png", 'wb')
-        mock_file().write.assert_any_call(b'fake image content')
-
-        # check zip file created (does not pass)
-        """mock_zipfile.assert_called_once()
-        mock_zipfile().__enter__().write.assert_any_call(
-            os.path.join(temp_dir, 'design_info_123.txt'), 'design_info_123.txt')
-        mock_zipfile().__enter__().write.assert_any_call(
-            os.path.join(temp_dir, 'image_1.png'), 'image_1.png')
-        mock_zipfile().__enter__().write.assert_any_call(
-            os.path.join(temp_dir, 'image_2.png'), 'image_2.png')"""
-
-        # heck temporary files cleaned up
-        self.assertEqual(mock_remove.call_count, 3)
-        mock_rmdir.assert_called_once_with(temp_dir)
-
-        self.assertIsInstance(result, BytesIO)
-
-    @patch('os.makedirs')
-    def test_createDesignZip_exception(self, mock_makedirs):
-        mock_makedirs.side_effect = Exception("Test exception")
-
-        design = {
-            'design_name': 'Test Design',
-            'design_id': '123',
-            'tags': ['tag1', 'tag2'],
-            'title': 'Test Title',
-            'related_links': ['http://link1.com', 'http://link2.com'],
-            'image_links': ['http://image1.png', 'http://image2.png']
-        }
-
-        with self.assertRaises(Exception):
-            createDesignZip(design)
+        result = deleteFromStorageByUrl(testUrl)
+        
+        self.assertFalse(result)
+        
