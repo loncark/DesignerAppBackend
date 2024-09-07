2ca8a3d020450fce57430cb012174c6491871662
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-09-07 13:18:37 +0200

    Error hahling cleanup

--- service/StableDiffusionService.py
+++ service/StableDiffusionService.py
@@ -1,5 +1,6 @@
-import aiohttp, asyncio
+import aiohttp
 from config import SD_TXT2IMG_URL, SD_IMG2IMG_URL
+import json
 
 async def txt2img(payload):
   return await call_api(SD_TXT2IMG_URL, payload)
@@ -9,21 +10,18 @@ async def img2img(payload):
   return await call_api(SD_IMG2IMG_URL, payload)
 
 
-async def call_api(url, payload, timeout=3000):  # 50 minutes timeout
+async def call_api(url, payload):  # 50 minutes timeout
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.post(url, json=payload, timeout=aiohttp.ClientTimeout(total=timeout)) as response:
+            async with session.post(url, json=payload, timeout=aiohttp.ClientTimeout(total=3000)) as response:
                 if response.status == 200:
                     return await response.json()
                 else:
                     print(f"Error: {response.status}")
-                    return None
-        except asyncio.TimeoutError:
-            print(f"Request timed out after {timeout} seconds")
-            return None
-        except aiohttp.ClientError as e:
-            print(f"Client error occurred: {e}")
-            return None
+                    return json.dumps({"Error": f"{response.status} Image generation failed."})
+        except Exception as e:
+            print(e)
+            return json.dumps({"Exception": e})
 
 
 
