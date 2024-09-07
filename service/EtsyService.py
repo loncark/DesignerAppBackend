27272bfd2dae3dd50f142abcfd833e7faaea3e5b
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-09-07 11:55:45 +0200

    Cleaned up

--- service/EtsyService.py
+++ service/EtsyService.py
@@ -1,29 +1,34 @@
 from config import RAPIDAPI_ETSY_URL, RAPIDAPI_API_KEY
 
-import requests, os, json
+import requests, json
 
-def fetchProducts(keyword, page):
+global_test = True
 
-    querystring = {"query": keyword, "page" : page}
+def fetchProducts(keyword, page, test=global_test):
+    if test:
+        if keyword == '':
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\EtsyPage1.json"
+        elif keyword == 'cat shirt':
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\EtsyPage2.json"
+        elif keyword == 'christmas':
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\EtsyPage3.json"
+        
+        with open(filePath, 'r') as file:
+            data = json.load(file)
+        
+        return data
 
-    headers = {
-        "x-rapidapi-key": RAPIDAPI_API_KEY,
-        "x-rapidapi-host": "etsy-api2.p.rapidapi.com"
-    }
+    else:
+        queryString = {"query": keyword, "page" : page}
 
-    response = requests.get(RAPIDAPI_ETSY_URL, headers=headers, params=querystring)
+        headers = {
+            "x-rapidapi-key": RAPIDAPI_API_KEY,
+            "x-rapidapi-host": "etsy-api2.p.rapidapi.com"
+        }
 
-    return response.json()
+        response = requests.get(RAPIDAPI_ETSY_URL, headers=headers, params=queryString)
 
-def fetchProducts2(keyword, page):
-    file_path = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\EtsyPage1.json"
+        return response.json()
     
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
-    
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    
-    return data
 
 
--- service/GoogleTrendsService.py
+++ service/GoogleTrendsService.py
@@ -1,158 +1,126 @@
-import serpapi, json, os
+import serpapi, json
 from config import SERPAPI_API_KEY
 from datetime import datetime, timedelta
 import requests, json
-from flask import jsonify, request
+from flask import jsonify
 
 # change frequency to realtime for realtime searches,
 # delete the date field and add "cat": "all",
 
-def fetchTrends(date, country_code):
-    params = {
-        "engine": "google_trends_trending_now",
-        "frequency": "daily",
-        "geo": country_code,
-        "date": date,
-        "api_key": SERPAPI_API_KEY
-    }
-    
-    try:
-        search = serpapi.search(params)
-        results = search.as_dict()
-        return results
-    except serpapi.SerpApiClientException as e:
-        if e.status_code == 429:
-            error_response = {
-                "message": "API limit of 100 searches per month exceeded."
-            }
-            return json.dumps(error_response), 429
-        else:
-            raise
+# LEAVE global_test=True TO NOT DRAIN THE API LIMIT (45/mo for etsy, a lot for trademark, 100/mo for Trends, interest and related together)
 
-def fetchRelatedQueries(keyword):
-    params = {
-        "engine": "google_trends",
-        "q": keyword,
-        "data_type": "RELATED_QUERIES",
-        "api_key": SERPAPI_API_KEY
-    }
-    
-    try:
-        search = serpapi.search(params)
-        results = search.as_dict()
-        return results
-    except serpapi.SerpApiClientException as e:
-        if e.status_code == 429:
-            error_response = {
-                "message": "API limit of 100 searches per month exceeded."
-            }
-            return json.dumps(error_response), 429
-        else:
-            raise
+global_test=False
 
-def fetchInterestByRegion(keyword):
-    params = {
-        "engine": "google_trends",
-        "q": keyword,
-        "data_type": "GEO_MAP_0",
-        "api_key": SERPAPI_API_KEY
-    }
-    
-    try:
-        search = serpapi.search(params)
-        results = search.as_dict()
-        return results
-    except serpapi.SerpApiClientException as e:
-        if e.status_code == 429:
-            error_response = {
-                "message": "API limit of 100 searches per month exceeded."
-            }
-            return json.dumps(error_response), 429
-        else:
-            raise
-
-def fetchInterestOverTime(keyword):
-    if not keyword:
-        return jsonify({'error': 'Keyword is required'}), 400
-
-    end_date = datetime.now().strftime('%Y-%m-%d')
-    start_date = (datetime.now() - timedelta(days=5*365)).strftime('%Y-%m-%d')
-
-    url = f"https://serpapi.com/search.json?engine=google_trends&q={keyword}&date={start_date} {end_date}&api_key={SERPAPI_API_KEY}"
-
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        data = response.json()
-
-        # Extract and process the timeline data
-        timeline_data = data['interest_over_time']['timeline_data']
+def fetchTrends(date, country_code, test=global_test):
+    if test:
+        filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\TrendPage1.json"   
         
-        processed_data = []
-        for item in timeline_data:
-            date = datetime.fromtimestamp(int(item['timestamp']))
-            value = item['values'][0]['extracted_value']
-            processed_data.append({
-                'date': date.strftime('%Y-%m-%d'),
-                'value': value
-            })
+        with open(filePath, 'r') as file:
+            data = json.load(file)
+        
+        return data
+    
+    else: 
+        params = {
+            "engine": "google_trends_trending_now",
+            "frequency": "daily",
+            "geo": country_code,
+            "date": date,
+            "api_key": SERPAPI_API_KEY
+        }
+        
+        try:
+            search = serpapi.search(params)
+            results = search.as_dict()
+            return results
+        except Exception as e:
+            if e.status_code == 429:
+                return json.dumps({"message": "API limit of 100 searches per month exceeded."}), 429
+            else:
+                print(e)
 
-        return jsonify({'data': processed_data})
-
-    except Exception as e:
-        print(f"Error processing data: {str(e)}")
-        return jsonify({'error': 'Failed to process trend data'}), 500
-
-
-# TEST FUNCTIONS TO NOT DRAIN THE API LIMIT (45/mo for etsy, a lot for trademark, 100/mo for Trends, interest and related together)
-def fetchTrends2(keyword, page):
-    file_path = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\TrendPage1.json"
-    
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
-    
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    
-    return data
-
-def fetchRelatedQueries2(keyword):
-    if(keyword == ''):
-        file_path = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsRelated.json"
-    else:
-        file_path = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsRelated2.json"
-
-    
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
-    
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    
-    return data
-
-def fetchInterestByRegion2(keyword):
-    if(keyword == ''):
-        file_path = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsInterest.json"
-    else:
-        file_path = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsInterest2.json"
-    
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"The file {file_path} does not exist.")
-    
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-    
-    return data
-
-def fetchInterestOverTime2(keyword):
-    try:
-        if (keyword == ''):
-            with open('C:\\Users\\Kristina\\Documents\\Diplomski rad\\DesignerAppBackend\\sample JSONs\\ChartData.json', 'r') as file:
-                data = json.load(file)
+def fetchRelatedQueries(keyword, test=global_test):
+    if test:
+        if(keyword == ''):
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsRelated.json"
         else:
-            with open('C:\\Users\\Kristina\\Documents\\Diplomski rad\\DesignerAppBackend\\sample JSONs\\ChartData2.json', 'r') as file:
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsRelated2.json"
+        
+        with open(filePath, 'r') as file:
+            data = json.load(file)
+    
+        return data
+
+    else:
+        params = {
+            "engine": "google_trends",
+            "q": keyword,
+            "data_type": "RELATED_QUERIES",
+            "api_key": SERPAPI_API_KEY
+        }
+        
+        try:
+            search = serpapi.search(params)
+            results = search.as_dict()
+            return results
+        except Exception as e:
+            if e.status_code == 429:
+                return json.dumps({"message": "API limit of 100 searches per month exceeded."}), 429
+            else:
+                print(e)
+
+def fetchInterestByRegion(keyword, test=global_test):
+    if test:
+        if(keyword == ''):
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsInterest.json"
+        else:
+            filePath = r"C:\Users\Kristina\Documents\Diplomski rad\DesignerAppBackend\sample JSONs\KeywordsInterest2.json"
+        
+        with open(filePath, 'r') as file:
+            data = json.load(file)
+        
+        return data
+
+    else:
+        params = {
+            "engine": "google_trends",
+            "q": keyword,
+            "data_type": "GEO_MAP_0",
+            "api_key": SERPAPI_API_KEY
+        }
+        
+        try:
+            search = serpapi.search(params)
+            results = search.as_dict()
+            return results
+        except Exception as e:
+            if e.status_code == 429:
+                return json.dumps({"message": "API limit of 100 searches per month exceeded."}), 429
+            else:
+                print(e)
+
+def fetchInterestOverTime(keyword, test=global_test):
+    try:
+        if test:
+            if (keyword == 'christmas'):
+                filePath = 'C:\\Users\\Kristina\\Documents\\Diplomski rad\\DesignerAppBackend\\sample JSONs\\ChartData.json'
+            else: 
+                filePath = 'C:\\Users\\Kristina\\Documents\\Diplomski rad\\DesignerAppBackend\\sample JSONs\\ChartData2.json'
+            
+            with open(filePath, 'r') as file:
                 data = json.load(file)
+
+        else:
+            if not keyword:
+                return jsonify({'error': 'Keyword is required'}), 400
+            
+            endDate = datetime.now().strftime('%Y-%m-%d')
+            startDate = (datetime.now() - timedelta(days=5*365)).strftime('%Y-%m-%d')
+
+            url = f"https://serpapi.com/search.json?engine=google_trends&q={keyword}&date={startDate} {endDate}&api_key={SERPAPI_API_KEY}"
+
+            response = requests.get(url)
+            data = response.json()
 
         timeline_data = data['interest_over_time']['timeline_data']
         
@@ -168,5 +136,5 @@ def fetchInterestOverTime2(keyword):
         return jsonify({'data': processed_data})
 
     except Exception as e:
-        print(f"Error processing data: {str(e)}")
-        return jsonify({'error': 'Failed to process trend data'}), 500
+        print("Error processing trend data: ", e)
+        return jsonify({'error': "Error processing trend data."})
