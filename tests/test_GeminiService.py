814e3693de33316f278d921c5b76eb26ed1c7b5e
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-09-19 12:33:05 +0200

    removed nonexistent function test cases

--- tests/test_GeminiService.py
+++ tests/test_GeminiService.py
@@ -1,6 +1,6 @@
 import unittest
 from unittest.mock import patch, Mock
-from service.GeminiService import fetchResponse, generateTags, generateTitle
+from service.GeminiService import fetchResponse
 
 class TestGeminiService(unittest.TestCase):
 
@@ -14,27 +14,3 @@ class TestGeminiService(unittest.TestCase):
         
         mockGenerateContent.assert_called_once_with("Tell me what cats are in one sentence.")
         self.assertEqual(result, "Cats are furry, domesticated mammals often kept as pets.")
-
-    @patch('google.generativeai.GenerativeModel.generate_content')
-    def test_generateTags(self, mockGenerateContent):
-        mockResponse = Mock()
-        mockResponse.text = "handmade, vintage, unique, colorful"
-        mockGenerateContent.return_value = mockResponse
-
-        result = generateTags("vintage necklace")
-        
-        expectedPrompt = """Generate me a list of tags I could use for Etsy SEO optimization of my "vintage necklace" listing. Separate these tags using a comma and write out nothing else."""
-        mockGenerateContent.assert_called_once_with(expectedPrompt)
-        self.assertEqual(result, ["handmade", "vintage", "unique", "colorful"])
-
-    @patch('google.generativeai.GenerativeModel.generate_content')
-    def test_generateTitle(self, mockGenerateContent):
-        mockResponse = Mock()
-        mockResponse.text = "Vintage Jewelry, Pretty Necklace, Cool Accessories"
-        mockGenerateContent.return_value = mockResponse
-
-        result = generateTitle("vintage necklace")
-        
-        expectedPrompt = """Generate me a list of synonyms I could use in the title of my "vintage necklace" listing of Etsy. They should be some phrases that people usually type in the search bar when looking for similar items. The first letter of every word in a synonym should be capitalized. Separate these tags using a comma and write out nothing else."""
-        mockGenerateContent.assert_called_once_with(expectedPrompt)
-        self.assertEqual(result, ["Vintage Jewelry", "Pretty Necklace", "Cool Accessories"])
