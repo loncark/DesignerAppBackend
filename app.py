9bcd230a6ce5ce6763dad88e226ea3fc03797dce
Kristina Lončar <48281855+loncark@users.noreply.github.com>
2024-09-24 13:54:15 +0200

    dynamic imports done

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 from flask import Flask
 from flask_cors import CORS
-from controller.FirebaseController import FirebaseController
-from controller.GeminiController import GeminiController
-from controller.TrademarkController import TrademarkController
-from controller.StableDiffusionController import StableDiffusionController
-from controller.GoogleTrendsController import GoogleTrendsController
-from controller.EtsyController import EtsyController
+import os
+import importlib       
 
 def createApp():
     app = Flask(__name__)
     CORS(app)
 
-    controllers = [
-        FirebaseController(),
-        GeminiController(),
-        TrademarkController(),
-        StableDiffusionController(),
-        GoogleTrendsController(),
-        EtsyController(),
-    ]
+    controllers = loadControllers()
 
     for controller in controllers:
         app.register_blueprint(controller.blueprint)
 
     return app
+
+def loadControllers():
+    rootDirPath = os.path.dirname(os.path.abspath(__file__))
+    controllerDirPath = os.path.join(rootDirPath, 'controller')
+    fileNames = os.listdir(controllerDirPath)
+
+    controllers = []
+    for fileName in fileNames:
+        if fileName.endswith('.py') and not fileName.startswith('__'):
+            controllerClassName = fileName[:-3]
+            module = importlib.import_module('controller.' + controllerClassName)
+
+            controllerClass = getattr(module, controllerClassName)
+            controllers.append(controllerClass())
+
+    return controllers
 
 if __name__ == '__main__':
     app = createApp()
